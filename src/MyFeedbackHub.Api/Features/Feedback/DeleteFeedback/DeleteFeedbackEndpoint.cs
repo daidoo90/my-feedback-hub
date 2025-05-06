05b5d0bbc6ae16e3a408ba6d97f6b7739abe40a3
Daniel <dani.damqnov.dimitrov@gmail.com>
2025-05-06 17:39:59 +0300

    Exposed endpoint for feedback deletion

---
+++ src/MyFeedbackHub.Api/Features/Feedback/DeleteFeedback/DeleteFeedbackEndpoint.cs
@@ -0,0 +1,34 @@
+﻿using MyFeedbackHub.Api.Shared.Utils;
+using MyFeedbackHub.Api.Shared.Utils.Carter;
+using MyFeedbackHub.Application.Feedback.DeleteFeedback;
+using MyFeedbackHub.Application.Shared.Abstractions;
+
+namespace MyFeedbackHub.Api.Features.Feedback.DeleteFeedback;
+
+public sealed class DeleteCommentEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/feedbacks/{feedbackId}", async (
+            Guid feedbackId,
+            ICommandHandler<DeleteFeedbackCommand> commandHandler,
+            CancellationToken cancellationToken) =>
+        {
+            var serviceResult = await commandHandler.HandleAsync(new DeleteFeedbackCommand(feedbackId), cancellationToken);
+
+            if (serviceResult.HasFailed)
+            {
+                return serviceResult.ToBadRequest("Deleting feedback failure");
+            }
+
+            return Results.Ok();
+        })
+        .WithName("DeleteFeedback")
+        .Produces(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete feedback")
+        .WithDescription("Delete feedback")
+        .WithTags("Feedback")
+        .RequireAuthorization();
+    }
+}
---
+++ src/MyFeedbackHub.Application/Feedback/DeleteFeedback/DeleteFeedbackCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFeedbackHub.Application.Shared.Abstractions;
+using MyFeedbackHub.SharedKernel.Results;
+
+namespace MyFeedbackHub.Application.Feedback.DeleteFeedback;
+
+public sealed record DeleteFeedbackCommand(Guid FeedbackId);
+
+internal class DeleteFeedbackCommandHandler(
+    IFeedbackHubDbContextFactory dbContextFactory,
+    IUserContext currentUser)
+    : ICommandHandler<DeleteFeedbackCommand>
+{
+    public async Task<ServiceResult> HandleAsync(DeleteFeedbackCommand command, CancellationToken cancellationToken = default)
+    {
+        var dbContext = await dbContextFactory.CreateAsync(cancellationToken);
+
+        var feedback = await dbContext.Feedbacks
+            .SingleOrDefaultAsync(f => f.FeedbackId == command.FeedbackId
+                                      && f.CreatedBy == currentUser.UserId, cancellationToken);
+
+        if (feedback == null || feedback.IsDeleted)
+        {
+            return ServiceResult.WithError(ErrorCodes.Feedback.FeedbackInvalid);
+        }
+
+        feedback.Delete(DateTimeOffset.UtcNow, currentUser.UserId);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return ServiceResult.Success;
+    }
+}
